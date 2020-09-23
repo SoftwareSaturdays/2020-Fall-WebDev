7ad2ceb501653d00db33ca22c89ec4b42c66c170
Hadi Ahmed <hadiahmed098@gmail.com>
2020-09-23 16:07:37 -0400

    Lesson2: Demo5 added

---
+++ demo5.js
@@ -0,0 +1,11 @@
+// Defines the onclick function for button
+function changepage(){
+    // Change the HTML text
+    let pTag = document.getElementById("pleasechange");
+    pTag.innerHTML = "This page was changed!";
+
+    let hTags = document.getElementsByClassName("heading");
+    for(let i = 0; i < hTags.length; ++i) {
+        hTags[i].style.color = "blue";
+    }
+}
