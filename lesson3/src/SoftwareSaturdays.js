474ed78c67a8cef97f5e140a59377ca3afda4221
Hadi Ahmed <hadiahmed098@gmail.com>
2020-10-03 17:58:00 -0400

    Lesson3: Demos complete

---
+++ lesson3/src/SoftwareSaturdays.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './SoftwareSaturdays.css';
+
+class SoftwareSaturdays extends React.Component {
+    constructor(props) {
+        super(props)
+        // We will cover this next week
+    }
+
+    render () {
+        // Type your code here...
+        return <div className="SSTest"><p>This is Software Saturdays</p><p>We are learning ReactJS.</p></div>;
+    }
+}
+
+
+export default SoftwareSaturdays;
