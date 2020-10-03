6f5311293a7ccf564e5542852e45fe7f9756b369
Hadi Ahmed <hadiahmed098@gmail.com>
2020-10-03 15:16:39 -0400

    Fixed starter code

--- lesson3/src/index.js
+++ lesson3/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-someJSX = null;
+const someJSX = null;
 
 ReactDOM.render(someJSX , document.getElementById('root'));
