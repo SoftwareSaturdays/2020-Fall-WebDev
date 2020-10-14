244e36d945e45c5eb090f3849466ced643272964
Hadi Ahmed <hadiahmed098@gmail.com>
2020-10-14 18:42:42 -0400

    Lesson5: Demos done

--- lesson5/src/App.js
+++ lesson5/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import Demo1 from './component/Demo1';
+import Demo2 from './component/Demo2';
+import Demo3 from './component/Demo3';
 
 function App() {
   return (
     // Type your website layout here...
+    <div id="demos">
+      {/* Demo #1 */}
+      <Demo1 />
+      <hr />
+      
+      {/* Demo #2 */}
+      <Demo2 numbers={[1,2,3,4,5]}/>
+      <hr />
 
+      {/* Demo #3 */}
+      <Demo3 />
 
-    
+    </div>
   );
 }
 
